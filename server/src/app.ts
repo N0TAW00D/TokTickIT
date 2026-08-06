a2c3a8564f12bd1a6eccc77af19f65ded673f5f0
Natthawat Primsirikunawut <primsirikunawutnatthawat@gmail.com>
2026-08-06 11:05:38 +0700

    feat: add health check endpoint to verify server status

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,4 +6,8 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
+app.get('/api/health', (req: Request, res: Response) => {
+  res.status(200).json({ message: 'Express Server is running!' });
+});
+
 export default app;
