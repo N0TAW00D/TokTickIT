0a92c9b3bd14f26cd57084c9165c088515b54159
Natthawat Primsirikunawut <primsirikunawutnatthawat@gmail.com>
2026-08-06 10:25:01 +0700

    refactor:  server structure and add testing framework
    - Updated package.json to replace default test script with vitest and added supertest for testing.
    - Created a new app.ts file to encapsulate the Express app setup, moving the app initialization from index.ts.
    - Added a new test file app.test.ts to test the root GET endpoint, ensuring it responds with 'Hello World!'.
    - Introduced vitest configuration file for setting up the testing environment.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,6 @@
-import express, { type Express, type Request, type Response } from 'express';
+import app from './app.ts';
 
-const app: Express = express();
 const port = 3000;
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
